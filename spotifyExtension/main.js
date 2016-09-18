d176f43bb6c4cee3bd0b87b218032750b49ca98c
chrissew <christoffer.wallenberg@gmail.com>
2016-09-18 21:02:45 +0200

    Play/stop track

--- spotifyExtension/main.js
+++ spotifyExtension/main.js
@@ -29,11 +29,22 @@
         return(value);
     };
 
+    ext.play_preview = function(preview_url) {
+        $('body').append('<audio id=track src=' + preview_url + ' crossorigin=anonymous></audio>');
+        $('#track').play();
+    };
+
+    ext.stop_preview = function(preview_url) {
+        $('#track').pause();
+    };
+
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
             ['R', 'Search for a %m.type containing %s', 'search_songs', 'track', 'Arcade fire'],
-            ['r', 'Get from variable %s from position %n property %m.properties', 'get_from_position', '    ', 0, 'name']
+            ['r', 'Get from variable %s from position %n property %m.properties', 'get_from_position', ' ', 0, 'name'],
+            [' ', 'Play preview from url %s', 'play_preview', ' '],
+            [' ', 'Stop preview', 'stop_preview]
         ],
         menus: {
             type: ['track', 'album', 'playlist'],
